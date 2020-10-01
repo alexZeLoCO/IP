4695553ec0d588a8688189441f6729178a1e62f2
alexZeLoCO <alejandro.rod.lop@gmail.com>
2020-10-01 15:21:06 +0200

    First commit
    Uploading all files

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld { 							//:Crea la clase
+	public static void main (String [] args) {
+		/* Esto es una anotación
+		 * aquí sigue la anotación
+		 */
+		System.out.print("Hello World");
+	}
+}
